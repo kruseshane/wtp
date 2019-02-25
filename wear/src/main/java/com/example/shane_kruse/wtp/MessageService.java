1a5bb6f07bbae70d4fd923c8977c44f93af3b24f
kruseshane <32747753+kruseshane@users.noreply.github.com>
2019-02-25 18:45:50 -0500

    Initial commit

---
+++ wear/src/main/java/com/example/shane_kruse/wtp/MessageService.java
@@ -0,0 +1,32 @@
+package com.example.shane_kruse.wtp;
+
+import android.content.Intent;
+import com.google.android.gms.wearable.MessageEvent;
+import android.support.v4.content.LocalBroadcastManager;
+import com.google.android.gms.wearable.WearableListenerService;
+
+public class MessageService extends WearableListenerService {
+
+    @Override
+    public void onMessageReceived(MessageEvent messageEvent) {
+
+        System.out.println("Message received");
+
+        //If the message’s path equals "/my_path"...//
+        if (messageEvent.getPath().equals("/my_path")) {
+
+            //...retrieve the message//
+            final String message = new String(messageEvent.getData());
+            Intent messageIntent = new Intent();
+            messageIntent.setAction(Intent.ACTION_SEND);
+            messageIntent.putExtra("message", message);
+
+            //Broadcast the received Data Layer messages locally//
+            LocalBroadcastManager.getInstance(this).sendBroadcast(messageIntent);
+        }
+        else {
+            super.onMessageReceived(messageEvent);
+        }
+    }
+
+}
